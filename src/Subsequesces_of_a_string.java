f4dac9c9175933672802e6fb8dea83a1fd1651f3
Uday Thorat <udaythorat0018@gmail.com>
2025-08-31 11:40:21 +0530

    inital commit

--- src/Subsequesces_of_a_string.java
+++ src/Subsequesces_of_a_string.java
@@ -2,6 +2,22 @@ public class Subsequesces_of_a_string
 {
 	public static void main(String[] args)
 	{
+		String a="abc";
+		Subsequences(a,0,"");
 
+	}
+	static void Subsequences(String a,int c,String newString){
+
+		if(c==a.length()){
+			System.out.println(newString);
+			return;
+		}
+
+		char ch =a.charAt(c);
+
+		//include the character -->
+		Subsequences(a,c+1,newString+ch);
+		//exclude the character-->
+		Subsequences(a,c+1,newString);
 	}
 }
